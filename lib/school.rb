f036dffdeb8e135335483e3f90660e5658009c7c
Egregious <dvdscheibel@yahoo.com>
2021-05-20 19:41:24 -0400

    commit tree 35b5a96ed53ad2b9f845569fe0de4937b7acd859

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
 # code here!
+class School
+
+    attr_accessor :roster
+
+    def initialize(school)
+        @school = school
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        roster[grade] ||= []
+        roster[grade] << name
+    end
+
+    def grade(grade)
+        roster[grade]
+    end
+
+    def sort
+        sorted_students = {}
+
+        roster.each {|grade, students| sorted_students[grade] = students.sort}
+
+        sorted_students
+    end
+
+end
